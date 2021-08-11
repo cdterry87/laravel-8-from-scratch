b184f59d2fae187613969a1e835a9caf6603cbea
Chase Terry <chase.terry87@gmail.com>
2021-08-10 23:20:26 -0500

    Added mailchimp api and setup a test route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,24 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 
+Route::get('ping', function () {
+  $mailchimp = new MailchimpMarketing\ApiClient();
+
+  $mailchimp->setConfig([
+    'apiKey' => config('services.mailchimp.key'),
+    'server' => 'us5'
+  ]);
+
+  // $response = $mailchimp->ping->get();
+  // $response = $mailchimp->lists->getAllLists();
+  $response = $mailchimp->lists->addListMember('3b616432f2', [
+    'email_address' => 'example@example.com',
+    'status' => 'subscribed'
+  ]);
+
+  ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('post/{post:slug}', [PostController::class, 'show'])->name('post');
 Route::post('post/{post:slug}/comments', [PostCommentsController::class, 'store']);

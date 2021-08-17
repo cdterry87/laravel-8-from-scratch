ac784a4e95136bce0dbea094a40ad2959364fe5e
Chase Terry <chase.terry87@gmail.com>
2021-08-16 22:39:05 -0500

    Added an endpoint to sign up for newsletter

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\PostCommentsController;
 
-Route::get('ping', function () {
+Route::post('newsletter', function () {
+  request()->validate(['email' => 'required|email']);
+  
   $mailchimp = new MailchimpMarketing\ApiClient();
 
   $mailchimp->setConfig([
@@ -16,12 +18,19 @@ Route::get('ping', function () {
 
   // $response = $mailchimp->ping->get();
   // $response = $mailchimp->lists->getAllLists();
-  $response = $mailchimp->lists->addListMember('3b616432f2', [
-    'email_address' => 'example@example.com',
-    'status' => 'subscribed'
-  ]);
+  try {
+    $response = $mailchimp->lists->addListMember('3b616432f2', [
+      'email_address' => request('email'),
+      'status' => 'subscribed'
+    ]);
+  } catch (\Exception $e) {
+    throw \Illuminate\Validation\ValidationException::withMessages([
+      'email' => 'This email could not be added to our newsletter list.'
+    ]);
+  }
 
-  ddd($response);
+  // ddd($response);
+  return redirect('/')->with('success', 'You are now signed up for our newsletter!');
 });
 
 Route::get('/', [PostController::class, 'index'])->name('home');
